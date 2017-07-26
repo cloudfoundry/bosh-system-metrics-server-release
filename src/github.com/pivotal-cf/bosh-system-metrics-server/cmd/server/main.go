1da8289e4f6aae3b8d4cc31aa72ed1949d820413
Joseph Rodriguez <jrodriguez@pivotal.io>
2017-07-26 10:26:24 -0600

    Updates packaging to include relevant dependencies
    [#147722569]
    
    Signed-off-by: Warren Fernandes <wfernandes@pivotal.io>

--- src/github.com/pivotal-cf/bosh-system-metrics-server/cmd/server/main.go
+++ src/github.com/pivotal-cf/bosh-system-metrics-server/cmd/server/main.go
@@ -2,9 +2,15 @@ package main
 
 import (
 	"bufio"
+	"flag"
+	"fmt"
+	"log"
 	"math/rand"
+	"net"
 	"os"
 	"time"
+
+	"google.golang.org/grpc"
 
 	"github.com/pivotal-cf/bosh-system-metrics-server/pkg/definitions"
 	"github.com/pivotal-cf/bosh-system-metrics-server/pkg/egress"
@@ -14,10 +20,21 @@ import (
 
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	port := flag.Int("port", 25595, "The port which the grpc metrics server will listen on")
+	flag.Parse()
+
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
 	messages := make(chan *definitions.Event)
 
 	i := ingress.New(bufio.NewReader(os.Stdin), unmarshal.Event, messages)
 	e := egress.NewServer(messages)
+
+	grpcServer := grpc.NewServer()
+	definitions.RegisterEgressServer(grpcServer, e)
 
 	stopReadingMessages := i.Start()
 	stopWritingMessages := e.Start()
@@ -26,4 +43,7 @@ func main() {
 		stopReadingMessages()
 		stopWritingMessages()
 	}()
+
+	log.Printf("bosh system metrics grpc server listening on %d\n", *port)
+	grpcServer.Serve(lis)
 }
